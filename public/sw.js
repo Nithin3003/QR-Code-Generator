b87f94f0dcaa62655d9d65e7b8e3d887cb49f719
nithin3003 <msnithin84@gmail.com>
2025-12-23 14:34:22 +0530

    Combine both PropellerAds service workers into single file with multiple zones

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,21 @@
+// PropellerAds Service Worker - Multiple Zones
+// Zone 1: 5gvci.com - 10364434
+// Zone 2: 3nbf4.com - 10364466
+
+// Primary configuration
 self.options = {
     "domain": "5gvci.com",
     "zoneId": 10364434
 }
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
 
+// Secondary configuration
+self.options2 = {
+    "domain": "3nbf4.com",
+    "zoneId": 10364466
+}
+
+self.lary = ""
+
+// Load both PropellerAds service worker scripts
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
